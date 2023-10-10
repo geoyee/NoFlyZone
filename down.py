57888dac15ee689a9bd106b303aa961f20764384
Yizhou Chen <geoyee@yeah.net>
2023-10-10 08:37:21 +0800

    Update with python3

--- down.py
+++ down.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 import json
-import urllib2
+import urllib3
+
+urllib3.disable_warnings()
 
 # GEO系统将动态覆盖全球各类飞行受限制的区域，飞行用户将实时获取相关受限资讯，包含但不限于机场、由于一些突发情况（如森林火灾、大型活动等）造成的临时限飞区域、一些永久禁止飞行的区域（如监狱、核工厂等）。
 # 此外，用户在部分区域例如野生保护区、人流密集的城镇等允许飞行的区域也可能收到飞行警示。以上这些无法完全自由飞行的区域，都统称为限飞区，包含了警示区、加强警示区、授权区、限高区、禁飞区等。
@@ -9,41 +11,41 @@ import urllib2
 for lng in range(70, 140, 4):
     for lat in range(4, 54, 4):
         url_temple = "https://flysafe-api.dji.com/api/qep/geo/feedback/areas/in_rectangle?ltlat=%s&ltlng=%s&rblat=%s&rblng=%s&zones_mode=flysafe_website&drone=%s&level=0,1,2,3,7,8,10"
-        # 包装头部
-        firefox_headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0',
-                           'connection': "close",
-                           'content-type': "application/json;charset=utf-8",
-                           'date': "Fri, 03 May 2019 09:04:11 GMT",
-                           'transfer-encoding': "chunked",
-                           'via': "nw, 1.1 c307613fe3146dad6950808dc74f82f6.cloudfront.net (CloudFront)",
-                           'x-amz-cf-id': "wXPZwrzfKzB_RqcNwuiR7uMdUNvfDlmDZFpnz7ARCiXUqIVWBgU7sA==",
-                           'x-cache': "Miss from cloudfront",
-                           'x-kunkka-proxy-latency': "0",
-                           'x-kunkka-upstream-latency': "35",
-                           'x-newrelic-app-data': "PxQFU1JaAQoFR1BWBgcDUFwJBBFORDQHUjZKA1ZLVVFHDFYPHiZcTQdZUwRIB1JaXxQSTFReWkkCXU9AAwAGTVYOQRFZXlEfVABWTFYOXwMKURgAFhMXUF8KFRBYXVsDFh5TSxYDEQxRDmcBV1ZAQFwJVQRDT3QHEGRSEhIRUVBEAQUjRVRWFSJeRHwEAAoBVTVdABoUBB4DSwlSBVQCVlQPD01LUhQRAQdXUwAFBl8GVQELUAVRQBwEWQ5LXWk=",
-                           'x-protected-by': "ibg-sec",
-                           'x-service-from': "geo_service",
-                           'x-service-loc': "",
-                           'x-service-version': "1.2.3"}
-        # 构建请求
-        # request = Request(url, headers=firefox_headers)
+
+        firefox_headers = {
+            "User-Agent": "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20100101 Firefox/23.0",
+            "connection": "close",
+            "content-type": "application/json;charset=utf-8",
+            "date": "Fri, 03 May 2019 09:04:11 GMT",
+            "transfer-encoding": "chunked",
+            "via": "nw, 1.1 c307613fe3146dad6950808dc74f82f6.cloudfront.net (CloudFront)",
+            "x-amz-cf-id": "wXPZwrzfKzB_RqcNwuiR7uMdUNvfDlmDZFpnz7ARCiXUqIVWBgU7sA==",
+            "x-cache": "Miss from cloudfront",
+            "x-kunkka-proxy-latency": "0",
+            "x-kunkka-upstream-latency": "35",
+            "x-newrelic-app-data": "PxQFU1JaAQoFR1BWBgcDUFwJBBFORDQHUjZKA1ZLVVFHDFYPHiZcTQdZUwRIB1JaXxQSTFReWkkCXU9AAwAGTVYOQRFZXlEfVABWTFYOXwMKURgAFhMXUF8KFRBYXVsDFh5TSxYDEQxRDmcBV1ZAQFwJVQRDT3QHEGRSEhIRUVBEAQUjRVRWFSJeRHwEAAoBVTVdABoUBB4DSwlSBVQCVlQPD01LUhQRAQdXUwAFBl8GVQELUAVRQBwEWQ5LXWk=",
+            "x-protected-by": "ibg-sec",
+            "x-service-from": "geo_service",
+            "x-service-loc": "",
+            "x-service-version": "1.2.3",
+        }
+
         ltlat = lat + 4
         rblat = lat
         ltlng = lng
         rblng = lng + 4
         url = url_temple % (ltlat, ltlng, rblat, rblng, "dji-mini-4-pro")
 
-        f = file("url.txt", "a+")  # 以追加的方式
-        f.write(url)
-        f.write("\n")  # 写完通过\n进行换行
-        f.close()
-        html = urllib2.urlopen(url)
-        # 获取数据
-        data = html.read()
+        with open("url.txt", "a+") as f:
+            f.write(url)
+            f.write("\n")
 
-        f = file("data.txt", "a+")  # 以追加的方式
-        f.write(data)
-        f.write("\n")  # 写完通过\n进行换行
-        f.close()
-        # 转换成字典数据
-        data_json = json.loads(data)
+        http = urllib3.PoolManager()
+        res = http.request("GET", url)
+        if res.status == 200:
+            data = res.data.decode("utf-8")
+
+            with open("data.txt", "a+", encoding="utf-8") as f:
+                f.write(data)
+                f.write("\n")
+            # data_json = json.loads(data)
